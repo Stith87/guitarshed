107ffacc74d00389bbb2010cc08c0b9684d7bdcb
Guitar Shed <guitarshednet@gmail.com>
2014-03-01 23:45:45 -0800

    fixed cms text area

--- app/assets/javascripts/comfortable_mexican_sofa/admin/application.js
+++ app/assets/javascripts/comfortable_mexican_sofa/admin/application.js
@@ -1,18 +1,3 @@
 // Overwrite this file in your application /app/assets/javascripts/comfortable_mexican_sofa/admin/application.js 
 
- //= require tinymce
- window.CMS.wysiwyg = function() {
-     tinymce.init({
-	    selector: "textarea",
-	    theme: "modern",
-	    plugins: ["code"],
-	    toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
-	    toolbar2: "print preview media | forecolor backcolor emoticons",
-	    image_advtab: true,
-	    templates: [
-	        {title: 'Test template 1', content: 'Test 1'},
-	        {title: 'Test template 2', content: 'Test 2'}
-	    ]
 
-	});
- };
